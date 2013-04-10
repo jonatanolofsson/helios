4374b46242757d5f607670678289d5074b8c1b39
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2013-04-10 22:01:27 +0200

    More parentheses in crc algorithm

--- crc.hpp
+++ crc.hpp
@@ -8,9 +8,9 @@ namespace os {
     inline U16 crc16(const U8* const data, const U32 len) {
         U16 crc = 0;
         for(U32 i = 0; i < len; ++i) {
-            crc  = (U8)(crc >> 8) | (crc << 8);
+            crc  = ((U8)(crc >> 8)) | (crc << 8);
             crc ^= data[i];
-            crc ^= (U8)(crc & 0xff) >> 4;
+            crc ^= ((U8)(crc & 0xff)) >> 4;
             crc ^= (crc << 8) << 4;
             crc ^= ((crc & 0xff) << 4) << 1;
         }

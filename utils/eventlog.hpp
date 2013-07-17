89122959f3b1126d9de70b78a57299880255dd06
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2013-07-17 23:29:30 +0200

    Added basic support for log level filtering

--- utils/eventlog.hpp
+++ utils/eventlog.hpp
@@ -5,7 +5,16 @@
 #include <mutex>
 #include <iostream>
 
-#define LOG_EVENT(id, lvl, e)    do { std::unique_lock<std::mutex> _eventLogLock__(os::internal::eventLogLock); *os::internal::eventLogStream << os::demangle(id) << " [" << lvl << "] : " << e << std::endl; } while(0);
+#ifndef MIN_LOG_LEVEL
+#define MIN_LOG_LEVEL 0
+#endif
+
+#define LOG_EVENT(id, lvl, e) do { \
+        if(lvl >= MIN_LOG_LEVEL) { \
+            std::unique_lock<std::mutex> _eventLogLock__(os::internal::eventLogLock); \
+            *os::internal::eventLogStream << os::demangle(id) << " [" << lvl << "] : " << e << std::endl; \
+        } \
+    } while(0)
 
 
 namespace os {
